ab8448374c731e56a853051bfdc905db75fea605
alperen elcik <alperenelcik@hotmail.com>
2023-03-31 11:27:48 +0200

    added a filter button with options ready to be used with dummy data

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React, { StyleSheet, Text, View } from "react-native";
+import FilterButton from "./filterButtonComponent";
 
 export default function App() {
 	return (
 		<View style={styles.container}>
 			<Text>Open up App.js to start working on your app!</Text>
 			<StatusBar style="auto" />
+			<FilterButton options={["Option 1", "Option 2", "Option 3"]} />
 		</View>
 	);
 }
---
+++ filterButtonComponent.js
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+import Icon from "react-native-vector-icons/AntDesign";
+import {
+	View,
+	Text,
+	TouchableOpacity,
+	StyleSheet,
+	FlatList,
+	Animated,
+} from "react-native";
+
+const FilterButton = ({ options }) => {
+	const [isFilterOpen, setIsFilterOpen] = useState(false);
+	const [selectedOptions, setSelectedOptions] = useState([]);
+	const [buttonWidth, setButtonWidth] = useState(new Animated.Value(100));
+
+	const handleFilterToggle = async () => {
+		setIsFilterOpen(!isFilterOpen);
+
+		if (!isFilterOpen) {
+			// change the button width to a larger value
+			Animated.timing(buttonWidth, {
+				toValue: 300,
+				duration: 10,
+				useNativeDriver: false,
+			}).start();
+		} else {
+			// change button to original value
+			Animated.timing(buttonWidth, {
+				toValue: 100,
+				duration: 10,
+				useNativeDriver: false,
+			}).start();
+		}
+	};
+
+	const handleOptionSelect = option => {
+		// Check if the option is already selected
+		if (selectedOptions.includes(option)) {
+			// If it is, remove it from the selectedOptions array
+			const newSelectedOptions = selectedOptions.filter(
+				item => item !== option
+			);
+			setSelectedOptions(newSelectedOptions);
+		} else {
+			// If it isn't, add it to the selectedOptions array
+			setSelectedOptions([...selectedOptions, option]);
+		}
+	};
+
+	return (
+		<View style={styles.container}>
+			<TouchableOpacity style={styles.button} onPress={handleFilterToggle}>
+				<Animated.View style={[{ width: buttonWidth }]} />
+				<View style={styles.filterContainer}>
+					<Icon
+						style={styles.filterButtonItem}
+						name="filter"
+						size={30}
+						color="black"
+					/>
+					<Text style={styles.filterButtonItem}>Filter</Text>
+				</View>
+			</TouchableOpacity>
+			{isFilterOpen && (
+				<View style={styles.dropdown}>
+					<FlatList
+						data={options}
+						renderItem={({ item }) => (
+							<TouchableOpacity
+								style={styles.option}
+								onPress={() => handleOptionSelect(item)}>
+								<Text>{item}</Text>
+								{selectedOptions.includes(item) && <Text>✔️</Text>}
+							</TouchableOpacity>
+						)}
+						keyExtractor={item => item}
+					/>
+				</View>
+			)}
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: "row",
+		alignItems: "center",
+	},
+	button: {
+		backgroundColor: "white",
+		padding: 10,
+		paddingLeft: 30,
+		paddingRight: 30,
+		borderRadius: 50,
+		marginRight: 10,
+		alignItems: "center",
+		justifyContent: "center",
+		borderColor: "black",
+		borderWidth: 1,
+	},
+	buttonText: {
+		color: "white",
+		textAlign: "center",
+		paddingBottom: 20,
+	},
+	dropdown: {
+		position: "absolute",
+		top: 50,
+		right: 10,
+		backgroundColor: "white",
+		borderRadius: 5,
+		shadowColor: "black",
+		shadowOpacity: 0.25,
+		shadowRadius: 5,
+		elevation: 5,
+		padding: 10,
+	},
+	option: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		padding: 5,
+		flex: 1,
+		width: 310,
+	},
+	filterContainer: {
+		flexDirection: "row",
+		alignItems: "center",
+	},
+	filterButtonItem: {
+		marginRight: 5,
+	},
+});
+
+export default FilterButton;
