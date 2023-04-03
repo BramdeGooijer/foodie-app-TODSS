0d9e18da3fda3272d495321d64e3205f5cc63130
alperen elcik <alperenelcik@hotmail.com>
2023-04-03 12:02:24 +0200

    fixed categories buttons and added a file for sizes and themes

--- filterButtonComponent.js
+++ filterButtonComponent.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import Icon from "react-native-vector-icons/AntDesign";
 import {
+	COLORS,
+	SIZES,
+} from "/home/alperen/vscode/foodie-app/foodie-app-2/theme.jsx";
+import {
 	View,
 	Text,
 	TouchableOpacity,
 	StyleSheet,
 	FlatList,
 	Animated,
+	SafeAreaView,
+	ScrollView,
 } from "react-native";
 
 const FilterButton = ({ options }) => {
@@ -34,19 +40,7 @@ const FilterButton = ({ options }) => {
 		}
 	};
 
-	// const handleOptionSelect = option => {
-	// 	// Check if the option is already selected
-	// 	if (selectedOptions.includes(option)) {
-	// 		// If it is, remove it from the selectedOptions array
-	// 		const newSelectedOptions = selectedOptions.filter(
-	// 			item => item !== option
-	// 		);
-	// 		setSelectedOptions(newSelectedOptions);
-	// 	} else {
-	// 		// If it isn't, add it to the selectedOptions array
-	// 		setSelectedOptions([...selectedOptions, option]);
-	// 	}
-	// };
+	const [cuisines, setCuisines] = useState(1);
 
 	return (
 		<View style={styles.container}>
@@ -62,7 +56,213 @@ const FilterButton = ({ options }) => {
 					<Text style={styles.filterButtonItem}>Filter</Text>
 				</View>
 			</TouchableOpacity>
-			{isFilterOpen && <View style={styles.dropdown}>{}</View>}
+			{isFilterOpen && (
+				<View style={styles.dropdown}>
+					<View style={styles.item}>
+						<Text style={styles.title}>CUISINES</Text>
+						<View style={styles.row}>
+							<TouchableOpacity
+								onPress={() => {
+									setCuisines(1);
+								}}
+								style={[
+									styles.category,
+									{
+										borderColor: cuisines === 1 ? COLORS.primary : COLORS.grey,
+									},
+								]}>
+								<Text
+									style={[
+										styles.subtitle,
+										{ color: cuisines === 1 ? COLORS.primary : COLORS.grey },
+									]}>
+									All
+								</Text>
+							</TouchableOpacity>
+							<TouchableOpacity
+								onPress={() => {
+									setCuisines(2);
+								}}
+								style={[
+									styles.category,
+									{
+										borderColor: cuisines === 2 ? COLORS.primary : COLORS.grey,
+									},
+								]}>
+								<Text
+									style={[
+										styles.subtitle,
+										{ color: cuisines === 2 ? COLORS.primary : COLORS.grey },
+									]}>
+									American
+								</Text>
+							</TouchableOpacity>
+							<TouchableOpacity
+								onPress={() => {
+									setCuisines(3);
+								}}
+								style={[
+									styles.category,
+									{
+										borderColor: cuisines === 3 ? COLORS.primary : COLORS.grey,
+									},
+								]}>
+								<Text
+									style={[
+										styles.subtitle,
+										{ color: cuisines === 3 ? COLORS.primary : COLORS.grey },
+									]}>
+									Asian
+								</Text>
+							</TouchableOpacity>
+							<TouchableOpacity
+								onPress={() => {
+									setCuisines(4);
+								}}
+								style={[
+									styles.category,
+									{
+										borderColor: cuisines === 4 ? COLORS.primary : COLORS.grey,
+									},
+								]}>
+								<Text
+									style={[
+										styles.subtitle,
+										{ color: cuisines === 4 ? COLORS.primary : COLORS.grey },
+									]}>
+									Burger
+								</Text>
+							</TouchableOpacity>
+							<TouchableOpacity
+								onPress={() => {
+									setCuisines(5);
+								}}
+								style={[
+									styles.category,
+									{
+										borderColor: cuisines === 5 ? COLORS.primary : COLORS.grey,
+									},
+								]}>
+								<Text
+									style={[
+										styles.subtitle,
+										{ color: cuisines === 5 ? COLORS.primary : COLORS.grey },
+									]}>
+									Chineese
+								</Text>
+							</TouchableOpacity>
+							<TouchableOpacity
+								onPress={() => {
+									setCuisines(6);
+								}}
+								style={[
+									styles.category,
+									{
+										borderColor: cuisines === 6 ? COLORS.primary : COLORS.grey,
+									},
+								]}>
+								<Text
+									style={[
+										styles.subtitle,
+										{ color: cuisines === 6 ? COLORS.primary : COLORS.grey },
+									]}>
+									Fast Food
+								</Text>
+							</TouchableOpacity>
+							<TouchableOpacity
+								onPress={() => {
+									setCuisines(7);
+								}}
+								style={[
+									styles.category,
+									{
+										borderColor: cuisines === 7 ? COLORS.primary : COLORS.grey,
+									},
+								]}>
+								<Text
+									style={[
+										styles.subtitle,
+										{ color: cuisines === 7 ? COLORS.primary : COLORS.grey },
+									]}>
+									Italian
+								</Text>
+							</TouchableOpacity>
+							<TouchableOpacity
+								onPress={() => {
+									setCuisines(8);
+								}}
+								style={[
+									styles.category,
+									{
+										borderColor: cuisines === 8 ? COLORS.primary : COLORS.grey,
+									},
+								]}>
+								<Text
+									style={[
+										styles.subtitle,
+										{ color: cuisines === 8 ? COLORS.primary : COLORS.grey },
+									]}>
+									Mexican
+								</Text>
+							</TouchableOpacity>
+							<TouchableOpacity
+								onPress={() => {
+									setCuisines(9);
+								}}
+								style={[
+									styles.category,
+									{
+										borderColor: cuisines === 9 ? COLORS.primary : COLORS.grey,
+									},
+								]}>
+								<Text
+									style={[
+										styles.subtitle,
+										{ color: cuisines === 9 ? COLORS.primary : COLORS.grey },
+									]}>
+									Pasta
+								</Text>
+							</TouchableOpacity>
+							<TouchableOpacity
+								onPress={() => {
+									setCuisines(10);
+								}}
+								style={[
+									styles.category,
+									{
+										borderColor: cuisines === 10 ? COLORS.primary : COLORS.grey,
+									},
+								]}>
+								<Text
+									style={[
+										styles.subtitle,
+										{ color: cuisines === 10 ? COLORS.primary : COLORS.grey },
+									]}>
+									Rice
+								</Text>
+							</TouchableOpacity>
+							<TouchableOpacity
+								onPress={() => {
+									setCuisines(11);
+								}}
+								style={[
+									styles.category,
+									{
+										borderColor: cuisines === 11 ? COLORS.primary : COLORS.grey,
+									},
+								]}>
+								<Text
+									style={[
+										styles.subtitle,
+										{ color: cuisines === 11 ? COLORS.primary : COLORS.grey },
+									]}>
+									Asian
+								</Text>
+							</TouchableOpacity>
+						</View>
+					</View>
+				</View>
+			)}
 		</View>
 	);
 };
@@ -91,15 +291,20 @@ const styles = StyleSheet.create({
 	},
 	dropdown: {
 		position: "absolute",
-		top: 50,
-		right: 10,
-		backgroundColor: "white",
-		borderRadius: 5,
+		top: 60,
+		left: 0,
+		backgroundColor: COLORS.lightGray4,
+		width: 300, // <-- set the width to 300
+		borderRadius: SIZES.radius,
+		padding: SIZES.padding,
+		elevation: 1,
 		shadowColor: "black",
-		shadowOpacity: 0.25,
-		shadowRadius: 5,
-		elevation: 5,
-		padding: 10,
+		shadowOpacity: 0.8,
+		shadowRadius: 2,
+		shadowOffset: {
+			height: 1,
+			width: 1,
+		},
 	},
 	option: {
 		flexDirection: "row",
@@ -115,6 +320,23 @@ const styles = StyleSheet.create({
 	},
 	filterButtonItem: {
 		marginRight: 5,
+	},
+
+	subtitle: {
+		color: COLORS.grey,
+		fontWeight: "700",
+		fontSize: SIZES.h4,
+	},
+	category: {
+		borderRadius: 15,
+		borderWidth: 2,
+		padding: 5,
+		paddingHorizontal: 10,
+	},
+	row: {
+		flex: 1,
+		flexWrap: "wrap",
+		flexDirection: "row",
 	},
 });
 
